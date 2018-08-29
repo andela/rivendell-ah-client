85a80e1fa2235b3f241234f938252ddc186c24ac
Chidiebere Oguejiofor <oguejiofor.chidiebere@andela.com>
2018-08-29 12:37:05 +0100

    chore(initialised-react-app): create basic app
    - install dependencies
    - configure webpack
    - configure babel
    - add html template to the app
    - create an App component
    - add scripts to package.json
    - add image-loader config to the app
    - adds scss config to the app
    - add config for development and production environment
    - added semantic-ui-react for styling
    - updates eslintrc config
    
    [Finishes #160056365]

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = (env) => {
+  const common = {
+    entry: path.join(__dirname, 'src', 'index.js'),
+    output: {
+      path: path.join(__dirname, './dist'),
+	  filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /.js$|.jsx$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+        {
+          test: /\.scss|\.sass|\.css/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.(png|jpg|jpeg)$/,
+          use: [{
+            loader: 'url-loader',
+            options: { limit: 30000 },
+          }],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'public', 'index.html'),
+      }),
+      new CleanWebpackPlugin(['./src/dist', 'build'], {
+        root: path.join(__dirname, 'src', 'dist'),
+        dry: false,
+        verbose: true,
+      })
+    ],
+  };
+  if (env.mode === 'development') {
+    return Object.assign(common, {
+      devtool: 'inline-source-map',
+      devServer: {
+        contentBase: './dist',
+        historyApiFallback: true,
+      },
+    });
+  }
+
+  if (env.mode === 'production') {
+    return Object.assign(common, {
+      devtool: 'source-map',
+    });
+  }
+  return common;
+};
