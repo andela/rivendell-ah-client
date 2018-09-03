c281e77f27702519928fffcd8fd7dc8f25eb9a1f
fattylee <fattylee.remod@gmail.com>
2018-09-03 10:20:24 +0100

    chore(Setup Redux): Create the appropriate folder/file structure for React/Redux workflow
    - install dependencies
    - add components, actions, reducers folder in src folder
    - add store.js file in the root folder
    - Integrate redux-promise-middleware
    - Integrate redux-saga
    - Integrate redux-thunk
    - write a custom error handling middleware
    
    [#1160056366]

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,14 @@
 import { combineReducers } from 'redux';
+<<<<<<< HEAD
 import helloWorld from './helloWorld';
 
 export default combineReducers({
   helloWorld,
 });
+=======
+import postReducer from './postReducer';
+
+export default combineReducers({
+  posts: postReducer,
+});
+>>>>>>> chore(Setup Redux): Create the appropriate folder/file structure for React/Redux workflow
