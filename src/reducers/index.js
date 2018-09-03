d0cadd504da311f482c0b01f97466086429df567
Innocent Ngene <ngeneinnocent@gmail.com>
2018-09-03 18:31:54 +0100

    chore(setup redux): integrate redux into the app - setup redux-thunk - setup redux-saga - setup redux-promise-middleware - create an error handle middleware [Delivers #160056366]

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,9 @@
 import { combineReducers } from 'redux';
-<<<<<<< HEAD
-import helloWorld from './helloWorld';
 
-export default combineReducers({
-  helloWorld,
-});
-=======
+import helloWorld from './helloWorld';
 import postReducer from './postReducer';
 
 export default combineReducers({
   posts: postReducer,
+  helloWorld,
 });
->>>>>>> chore(Setup Redux): Create the appropriate folder/file structure for React/Redux workflow
