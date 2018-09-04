961816dd92b53c6a3fd482e196efca0e89a5a3af
Chidiebere Oguejiofor <oguejiofor.chidiebere@andela.com>
2018-09-04 13:22:18 +0100

    chore(setup-test-coverage): setup test coverage
    - installed jest and enzyme
    - added jest.config to the app
    - updates circleCI config
    - adds project to codeclimate
    - ensures that build is passing on circleci
    - seperates script for test and coverage
    - adds repo token environmental variable to circleCI
    
    [#160056368, #160056367, #160056371]

--- tests/config/setup.js
+++ tests/config/setup.js
@@ -1,4 +1,11 @@
+<<<<<<< HEAD
 import enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 enzyme.configure({ adapter: new Adapter() });
+=======
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+>>>>>>> chore(setup-test-coverage): setup test coverage
