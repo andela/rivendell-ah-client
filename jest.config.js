aff2eea18d36c9d9889d5a93256b4b19b30d6086
Chidiebere Oguejiofor <oguejiofor.chidiebere@andela.com>
2018-09-04 13:22:18 +0100

    chore(setup-test-coverage): setup test coverage
    - installed jest and enzyme
    - added jest.config to the app
    - updates circleCI config
    - adds project to codeclimate
    - ensures that build is passing on circleci
    - seperates script for test and coverage
    - adds repo token environmental variable to circleCI
    
    [#160056368, #160056367, #160056371]

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/* eslint max-len:off */
+module.exports = {
+  setupFiles: [
+    '<rootDir>/tests/config/setup.js',
+  ],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{js,jsx}'],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/tests',
+    '<rootDir>/src/index.js',
+  ],
+  testURL: 'http://localhost',
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'identity-obj-proxy',
+    '\\.(css|scss)$': 'identity-obj-proxy',
+  },
+};
