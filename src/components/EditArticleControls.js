a3bb59b89a845553c866a428d303c20075b6b32f
Chidiebere Oguejiofor <oguejiofor.chidiebere@andela.com>
2018-09-07 14:38:54 +0100

    finally completed the general features

--- src/components/EditArticleControls.js
+++ src/components/EditArticleControls.js
@@ -1,36 +1,114 @@
-import React from 'react';
-import { Button, Dropdown } from 'semantic-ui-react';
+import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+
+import CategoryInput from './CategoryInput';
+import TagInput from './TagInput';
 
 
-const options = [
-  { key: 'English', text: 'English', value: 'English' },
-  { key: 'French', text: 'French', value: 'French' },
-  { key: 'Spanish', text: 'Spanish', value: 'Spanish' },
-  { key: 'German', text: 'German', value: 'German' },
-  { key: 'Chinese', text: 'Chinese', value: 'Chinese' },
-];
-const EditControls = () => (
-  <Button.Group>
-    <Dropdown
-      options={options}
+/**
+ * This component manages the state of the input elements
+ * to the edit Articles
+ * @class EditControls
+ * @returns {object} instance of an object
+ * @param {object} e
+ */
+class EditControls extends Component {
+  /**
+   *This instantiates the state of this component
+   * @param {object} props the properties passed to it as argument
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentValues: [],
+      tags: [
+        { key: 'English', text: 'English', value: 'English' },
+        { key: 'French', text: 'French', value: 'French' },
+        { key: 'Spanish', text: 'Spanish', value: 'Spanish' },
+      ],
+    };
 
-      search
-      selection
-      multiple
-      allowAdditions
-    />
+    this.categories = [
+      {
+        name: 'OTHERS',
+        subcategories: [
+          {
+            id: 1,
+            name: 'OTHERS'
+          }
+        ]
+      },
+      {
+        name: 'TECH',
+        subcategories: [
+          {
+            id: 2,
+            name: 'ENGINEERING'
+          },
+          {
+            id: 3,
+            name: 'SOFTWARE DEVELOPMENT'
+          },
+          {
+            id: 4,
+            name: 'ARTIFICIAL INTELLIGENCE'
+          },
+          {
+            id: 5,
+            name: 'BIOTECHNOLOGY'
+          },
+          {
+            id: 6,
+            name: 'NANO TECHNOLOGY'
+          }
+        ]
+      },
+    ];
+  }
 
-  </Button.Group>
+
+  /**
+   *this function is called when a new field is inserted
+    to the tags in while the user is creating articles
+  @returns {void} this function performs an action and
+  thus returns nothig
+  @param {object} e the event handler object
+   */
+  handleAddition = (e, { value }) => {
+    const { tags } = this.state;
+    this.setState({
+      tags: [{ key: value, text: value, value }, ...tags],
+    });
+  }
+
+  /**
+   *this function is called when the value of the
+  tag changes
+  @returns {void} this function performs an action and
+  thus returns nothig
+  @param {object} e the event handler object
+   */
+  handleChange = (e, { value }) => this.setState({ currentValues: value })
 
 
-);
-
-// {/* {buttons instanceof Array ? buttons.map((button, index) => (
-//       <Button
-//         content={button.content}
-//         key={index}
-//         secondary={!!button.secondary}
-//         primary={!!button.primary}
-//         positive={!!button.positive}
-//       />)) : null} */}
+  /**
+   * This returns a group of custom components that
+   *@returns {JSX} a JSX to be rendered
+   */
+  render() {
+    const { tags, currentValues } = this.state;
+    return (
+      <div>
+        <TagInput
+          options={tags}
+          hadleAddition={this.handleAddition}
+          handleChange={this.handleChange}
+          currentValues={currentValues}
+        />
+        <CategoryInput categories={this.categories} />
+        <Button primary content="Create Article" />
+      </div>
+    );
+  }
+}
 export default EditControls;
--- tests/controllers/EditArticleControls.test.js
+++ tests/controllers/EditArticleControls.test.js
@@ -3,58 +3,28 @@
 import { shallow } from 'enzyme';
 import React from 'react';
 import chai from 'chai';
-import { Button, Dropdown } from 'semantic-ui-react';
 import EditArticleControls from '../../src/components/EditArticleControls';
+import CategoryInput from '../../src/components/CategoryInput';
+import TagInput from '../../src/components/TagInput';
 
 describe('Testing EditArticleControls', () => {
   let mountedObj;
 
 
   describe('basic elements rendered in the component', () => {
-    it('should contain a semantic-ui-react Button.Group Component', () => {
+    it('should contain a CategoryInput', () => {
       mountedObj = shallow(<EditArticleControls />);
-      chai.expect(mountedObj.find(Button.Group).length).to.be.greaterThan(0);
+      chai.expect(mountedObj.find(CategoryInput).length)
+        .to.equal(1);
     });
-
-    it('should contain a semantic-ui-react Dropdowm', () => {
+    it('should contain a TagInput', () => {
       mountedObj = shallow(<EditArticleControls />);
-      chai.expect(mountedObj.find(Dropdown).length).to.be.greaterThan(0);
+      chai.expect(mountedObj.find(TagInput).length)
+        .to.equal(1);
     });
   });
-
 
   describe('the controls contained in the component', () => {
 
   });
-
-  // describe('when the button props is added to it', () => {
-  //   // const buttonsArr = [
-  //   //   {
-  //   //     primary: true,
-  //   //     content: 'First Button',
-  //   //     positive: true
-  //   //   },
-  //   //   {
-  //   //     secondary: true,
-  //   //     content: 'Second Button'
-  //   //   },
-  //   // ];
-  //   // it('should render button content based on props from array', () => {
-  //   //   mountedObj = shallow(<EditArticleControls buttons={buttonsArr} />);
-  //   //   const allValid = buttonsArr.every(button => mountedObj.find(
-  //   //     `Button[content="${button.content}"]`
-  //   //   ).length > 0);
-
-  //   //   chai.expect(allValid).to.equal(true);
-  //   // });
-
-  //   // it('should render button types based on props from array', () => {
-  //   //   mountedObj = shallow(<EditArticleControls buttons={buttonsArr} />);
-  //   //   const allValid = buttonsArr.every(button => mountedObj.find(
-  //   //     `${button.primary ? '[primary=true]' : ''}${button.secondary ? '[secondary=true]' : ''}`
-  //   //   ).length > 0);
-
-  //   //   chai.expect(allValid).to.equal(true);
-  //   // });
-  // });
 });
