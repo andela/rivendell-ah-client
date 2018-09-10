34be146cc6b6f49557c640590913fa7d11d69ab5
tutugodfrey <godfrey_tutu@yahoo.com>
2018-09-10 14:37:00 +0100

    feat(axios-instance): set up axios instance
    - install axois
    - define a function to start an axios instance
    - define an example function for making api requests
    
    [Delivers #160283233]

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import axiosInstance from './services/requestHandler';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './views/Main';
+
+/**
+ * example function of how to make http request
+ * using the axios instance
+ * @returns { Object } response from server
+ */
+/* eslint-disable class-methods-use-this */
+const makeRequest = () => {
+  /* eslint-disable no-console */
+  axiosInstance().get('/articles')
+    .then(res => console.log(res.data));
+};
 
 const App = () => (
   <div>
     <Header />
     <Main />
+    <button onClick={makeRequest}>Get articles</button>
     <Footer />
   </div>
 );
