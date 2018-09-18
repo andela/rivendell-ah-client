527ca211eef46a401881b1e96a69cebc05aae4bf
tutugodfrey <30448913+tutugodfrey@users.noreply.github.com>
2018-09-18 04:15:39 +0100

    chore(token-header): add token header (#20)
    added token to axios instannce
    
    [Finishes #160552899]

--- src/services/requestHandler.js
+++ src/services/requestHandler.js
@@ -10,8 +10,14 @@ const axiosInstance = () => {
   if (process.env.NODE_ENV === 'development') {
     apiUrl = 'http://localhost:3000/api';
   }
+
+  let token = '';
+  if (localStorage.getItem('token')) {
+    token = localStorage.getItem('token');
+  }
   const headers = new Headers();
   headers.append('Content-Type', 'application/json');
+  headers.append('Authorization', token);
   const instanceCreate = axios.create({
     baseURL: apiUrl,
     headers,
